91105ee3a4e8d0f2dfbb938f5bd03c1f7287e6b2
ahertzog <alon.hertzog@hexagon.com>
2025-08-19 22:55:27 +0300

    change to card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '../components/utils';
 import { Button } from '../components/ui/button';
 import { Heart, Users, Brain, BookOpen } from 'lucide-react';
-import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/Card';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import WhatsappIcon from '../components/WhatsappIcon';
 
 export default function Home() {
--- src/pages/lectures.jsx
+++ src/pages/lectures.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from '../components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/Card';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import { Users, BookOpen, Heart, Brain, Clock, Target } from 'lucide-react';
 import WhatsappIcon from '../components/WhatsappIcon';
 
