048572c6e61617a367c95fdffba13671ea1a1c3b
ahertzog <alon.hertzog@hexagon.com>
2025-09-15 22:07:43 +0300

    syntax

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 export function createPageUrl(pageName) {
   const pageMap = {
-    'Home': '/',
-    'About': '/about',
-    'IndividualTherapy': '/individual-therapy',
-    'ParentGuidance': '/parent-guidance',
-    'BodyMindTherapy': '/body-mind-therapy',
-    'Clinic': '/clinic',
-    'Lectures': '/lectures',
-    'Contact': '/contact'
+    Home: '/',
+    About: '/about',
+    IndividualTherapy: '/individual-therapy',
+    ParentGuidance: '/parent-guidance',
+    BodyMindTherapy: '/body-mind-therapy',
+    Clinic: '/clinic',
+    Lectures: '/lectures',
+    Contact: '/contact'
   };
 
   return pageMap[pageName] || '/';
