f05ef89cb945092609f775fa6d427ae296b21e79
ahertzog <alon.hertzog@hexagon.com>
2025-11-09 12:07:46 +0200

    fix utils

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,8 +2,8 @@ export function createPageUrl(pageName) {
   const pageMap = {
     Home: '/',
     About: '/About',
-    IndividualTherapy: '/IndividualTherapy',
-    ParentGuidance: '/ParentGuidance',
+    IndividualTherapy: '../IndividualTherapy',
+    ParentGuidance: '../ParentGuidance',
     Clinic: '/Clinic',
     Lectures: '/lectures',
     Contact: '/contact',
